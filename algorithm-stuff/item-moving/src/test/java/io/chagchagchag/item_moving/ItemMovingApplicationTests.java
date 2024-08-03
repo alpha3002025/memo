4caa376058a84a5712c1ac613cf924f1554a7b54
chagchagchag <chagchagchag.dev@gmail.com>
2024-08-04 00:01:26 +0900

    Feat : Item Moving

---
+++ algorithm-stuff/item-moving/src/test/java/io/chagchagchag/item_moving/ItemMovingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.item_moving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemMovingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
