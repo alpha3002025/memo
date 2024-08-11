d2010ba384ef662be1eea456682c97ec500d092a
chagchagchag <chagchagchag.dev@gmail.com>
2024-08-11 19:12:28 +0900

    Feat : prometheus + grafana
    Example First Commit

---
+++ grafana-prometheus/example/prometheus_grafana_example/src/main/java/io/chagchagchag/example/prometheus_grafana_example/PrometheusGrafanaExampleApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.prometheus_grafana_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusGrafanaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusGrafanaExampleApplication.class, args);
+	}
+
+}
---
+++ grafana-prometheus/example/prometheus_grafana_example/src/test/java/io/chagchagchag/example/prometheus_grafana_example/PrometheusGrafanaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.prometheus_grafana_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusGrafanaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
