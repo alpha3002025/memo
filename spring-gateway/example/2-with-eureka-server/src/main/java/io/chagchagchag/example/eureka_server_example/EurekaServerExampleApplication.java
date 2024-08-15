150dc99812a27904efea74bc81435afb72a0b210
chagchagchag <chagchagchag.dev@gmail.com>
2024-08-15 19:37:54 +0900

    Feat : Gateway Example (with Eureka Server)

---
+++ spring-gateway/example/2-with-eureka-server/src/main/java/io/chagchagchag/example/eureka_server_example/EurekaServerExampleApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.eureka_server_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerExampleApplication.class, args);
+	}
+
+}
---
+++ spring-gateway/example/2-with-eureka-server/src/test/java/io/chagchagchag/example/eureka_server_example/EurekaServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.eureka_server_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
