540ba290e47dbb2cadb56711b2e8ad94a994455a
chagchagchag <chagchagchag.dev@gmail.com>
2024-08-18 16:39:28 +0900

    Feat : Gateway + Eureka + MSA

--- spring-gateway/example/gateway-eureka-msa-services/service-discovery/src/main/java/io/chagchagchag/example/service_discovery/ServiceDiscoveryApplication.java
+++ spring-gateway/example/gateway-eureka-msa-services/service-discovery/src/main/java/io/chagchagchag/example/service_discovery/ServiceDiscoveryApplication.java
@@ -2,7 +2,9 @@ package io.chagchagchag.example.service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
