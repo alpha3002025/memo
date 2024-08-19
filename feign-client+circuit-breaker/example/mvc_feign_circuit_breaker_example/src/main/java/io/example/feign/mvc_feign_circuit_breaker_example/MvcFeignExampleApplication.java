ecda8cc068a5730ee5b648d0a60cfc263b063e0d
chagchagchag <chagchagchag.dev@gmail.com>
2024-08-20 07:29:16 +0900

    Feat : feigin example

---
+++ feign-client+circuit-breaker/example/mvc_feign_circuit_breaker_example/src/main/java/io/example/feign/mvc_feign_circuit_breaker_example/MvcFeignExampleApplication.java
@@ -0,0 +1,14 @@
+package io.example.feign.mvc_feign_circuit_breaker_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableFeignClients
+@SpringBootApplication
+public class MvcFeignExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcFeignExampleApplication.class, args);
+	}
+
+}
