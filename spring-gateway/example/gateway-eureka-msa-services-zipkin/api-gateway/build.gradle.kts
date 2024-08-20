75d2b199b93d6008a2268b215dff537937e4d842
chagchagchag <chagchagchag.dev@gmail.com>
2024-08-21 08:24:00 +0900

    Feat : Zipkin 연동

--- spring-gateway/example/gateway-eureka-msa-services-zipkin/api-gateway/build.gradle.kts
+++ spring-gateway/example/gateway-eureka-msa-services-zipkin/api-gateway/build.gradle.kts
@@ -30,6 +30,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
+	// zipkin
+	// spring 3.x 부터 deprecated
+//	implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+	implementation("io.micrometer:micrometer-tracing-bridge-brave")
+	implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
