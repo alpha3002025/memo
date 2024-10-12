114ae0551d100c3346969ce6befea442eacbaa29
chagchagchag <chagchagchag.dev@gmail.com>
2024-10-12 18:28:00 +0900

    first status

---
+++ algorithm-stuff/examples/item_moving_redis/src/main/java/io/chagchagchag/example/item_moving_redis/ItemMovingRedisApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.item_moving_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemMovingRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemMovingRedisApplication.class, args);
+	}
+
+}
---
+++ algorithm-stuff/examples/item_moving_redis/src/test/java/io/chagchagchag/example/item_moving_redis/ItemMovingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.item_moving_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemMovingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
