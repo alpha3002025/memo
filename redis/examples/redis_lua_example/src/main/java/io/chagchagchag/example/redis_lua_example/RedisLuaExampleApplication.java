e45f4b1ac43bc4ad8741495f57101cad7178c0fb
chagchagchag <chagchagchag.dev@gmail.com>
2024-10-19 16:52:04 +0900

    Feat : Redis, Lua at Spring Boot

---
+++ redis/examples/redis_lua_example/src/main/java/io/chagchagchag/example/redis_lua_example/RedisLuaExampleApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.redis_lua_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLuaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLuaExampleApplication.class, args);
+	}
+
+}
