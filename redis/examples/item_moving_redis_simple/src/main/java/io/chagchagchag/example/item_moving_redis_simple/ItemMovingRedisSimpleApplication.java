aef9b0ab66413db2c85f52902281816324cec189
chagchagchag <chagchagchag.dev@gmail.com>
2024-10-20 16:32:38 +0900

    Initial Commit : ItemMoving Redis Lua

---
+++ redis/examples/item_moving_redis_simple/src/main/java/io/chagchagchag/example/item_moving_redis_simple/ItemMovingRedisSimpleApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.item_moving_redis_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemMovingRedisSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemMovingRedisSimpleApplication.class, args);
+	}
+
+}
---
+++ redis/examples/item_moving_redis_simple/src/test/java/io/chagchagchag/example/item_moving_redis_simple/ItemMovingRedisSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.item_moving_redis_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemMovingRedisSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
